9157c1fb5e3161c83968a9808ae8b46d6ae3ab0d
Leandro da Silva de Souza <leandro.souza.159@gmail.com>
2023-07-05 18:17:42 -0300

    Implementado métodos estáticos

--- conta.py
+++ conta.py
@@ -1,20 +1,27 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        print("Construindo objeto...{}".format(self))
+        print(f"Construindo objeto...{self}")
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
 
     def extrato(self):
-        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+        print(f"Saldo de {self.__saldo} do titular {self.__titular}")
 
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print(f"O valor {valor} passou o limite")
 
     def transfere(self, valor, destino):
         self.saca(valor)
@@ -33,3 +40,11 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
